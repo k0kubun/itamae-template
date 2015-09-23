db0ebabc7238ab04cdf1bdca9d82b2ca1f82d667
Takashi Kokubun <takashikkbn@gmail.com>
2015-09-23 17:53:41 +0900

    Fix a comment

--- template/config/deploy.rb
+++ template/config/deploy.rb
@@ -1,8 +1,8 @@
-# TODO: Configure hosts to apply recipes.
-#
 # You can filter roles by ROLES env variable.
 #   $ ROLES=role1,role2 bundle exec cap itamae apply
 #
 # With following example, you can apply recipes to production role by:
 #   $ ROLES=production bundle exec cap itamae apply
+#
+# TODO: Configure hosts to apply recipes.
 role :production, %w[example-host.com]
