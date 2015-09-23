362338b12268563a5b14f65807796d55c684b7b7
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-09-23 12:57:24 +0900

    bundle gem itamae-template

---
+++ lib/itamae/template.rb
@@ -0,0 +1,7 @@
+require "itamae/template/version"
+
+module Itamae
+  module Template
+    # Your code goes here...
+  end
+end
---
+++ lib/itamae/template/version.rb
@@ -0,0 +1,5 @@
+module Itamae
+  module Template
+    VERSION = "0.1.0"
+  end
+end
